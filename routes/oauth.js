c41e11fb8743d92c2384ae3cfcbfc8678699bd48
Jack <jackmurphy756@gmail.com>
2021-08-13 21:46:10 +0100

    Print out error messages

--- routes/oauth.js
+++ routes/oauth.js
@@ -9,11 +9,12 @@ const postF = Future.encaseP(axios.post);
 
 
 
-module.exports = (credentials, database, successAddress) => req => {
+module.exports = (credentials, database, successAddress) => (req, response )=> {
     const code = req.query.code;
     const userId = req.ip;
     // const userId = req.get("X-USER-ID");
     // console.log("userId ", userId)
+
     const url =
     `https://api.coinbase.com/oauth/token` +
     `?grant_type=authorization_code` +
@@ -28,18 +29,14 @@ module.exports = (credentials, database, successAddress) => req => {
       "Access-Control-Allow-Origin": "*",
       "CB-Version": "2019-12-12"
     }
-    };
-    
+    }; 
+
     return postF(url, options)
-    // .pipe(map(r => {console.log(r); return r;}))    
     .pipe(map(res => {
-            console.log(1, url, 2, options); 
             database[userId] = {
                 accessToken: res.body.access_token,
                 refresh_token: res.body.refresh_token
             };
-            console.log("body: ", res.body);
-            console.log("DB: ", database);
             return res;
         }))
         .pipe(map(_ => purifier.respond.redirect({ path: successAddress })));
