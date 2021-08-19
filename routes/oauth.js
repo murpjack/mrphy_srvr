2027c93221712759c1c3968754a979961c32dfb4
Jack <jackmurphy756@gmail.com>
2021-08-19 21:54:53 +0100

    refactor

--- routes/oauth.js
+++ routes/oauth.js
@@ -7,21 +7,7 @@ module.exports = (credentials, database, successAddress) => (req, res) => {
   const code = req.query.code;
   const userId = req.ip;
 
-  const url =
-    "https://api.coinbase.com/oauth/token" +
-    `?grant_type=authorization_code` +
-    `&code=${code}` +
-    `&client_id=${credentials.clientId}` +
-    `&client_secret=${credentials.clientSecret}` +
-    `&redirect_uri=${credentials.successUri}`;
-
-  //   const body = {
-  //     grant_type: `authorization_code`,
-  //     code: code,
-  //     client_id: credentials.clientId,
-  //     client_secret: credentials.clientSecret,
-  //     redirect_uri: credentials.successUri,
-  //   };
+  const url = "https://api.coinbase.com/oauth/token";
 
   const options = {
     method: "POST",
@@ -30,20 +16,29 @@ module.exports = (credentials, database, successAddress) => (req, res) => {
       "Access-Control-Allow-Origin": "*",
       "CB-Version": "2019-12-12",
     },
-    // body: JSON.stringify(body),
+    body: JSON.stringify({
+      grant_type: `authorization_code`,
+      code: code,
+      client_id: credentials.clientId,
+      client_secret: credentials.clientSecret,
+      redirect_uri: credentials.successUri,
+    }),
   };
 
   //   return Future.encaseP(fetch)("https://api.github.com", {})
-  return Future.encaseP(fetch)(url, options)
-    .pipe(chain(encaseP((res) => res.json())))
-    .pipe(
-      map((body) => {
-        database[userId] = {
-          accessToken: body.access_token || "123",
-          refresh_token: body.refresh_token || "456",
-        };
-        return body;
-      })
-    )
-    .pipe(map((_) => purifier.respond.redirect({ path: successAddress })));
+  return (
+    Future.encaseP(fetch)(url, options)
+      // .pipe(chain(encaseP((res) => res.json())))
+      .pipe(
+        map((body) => {
+          database[userId] = {
+            accessToken: body.access_token || "123",
+            refresh_token: body.refresh_token || "456",
+          };
+          console.log({ body });
+          return body;
+        })
+      )
+      .pipe(map((_) => purifier.respond.redirect({ path: successAddress })))
+  );
 };
