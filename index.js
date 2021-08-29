a633fe3bebe3ccec9b99fb6607da59130da31c54
Jack <jackmurphy756@gmail.com>
2021-08-29 20:32:14 +0100

    POST for token error - not a valid request

--- index.js
+++ index.js
@@ -1,27 +1,22 @@
-require("dotenv").config({ path: "./.env" });
-const cors = require("cors");
-const Client = require("coinbase").Client;
-const credentials = require("root-require")("./credentials");
-const purifier = require("root-require")("./lib/routePurifier");
-const express = require("express");
-const routes = require("require-dir-all")("./routes", {
+require('dotenv').config({ path: './.env' });
+const helmet = require('helmet');
+const cors = require('cors');
+const credentials = require('root-require')('./credentials');
+const purifier = require('root-require')('./lib/routePurifier');
+const express = require('express');
+const routes = require('require-dir-all')('./routes', {
   recursive: true,
 });
-
-const fetch = require("node-fetch");
-const Future = require("fluture");
-const { map, chain, encaseP, fork } = Future;
 
 const PORT = process.env.port || 4001;
 const app = express();
 
 /** Express server/web best practices. */
-const helmet = require("helmet");
 app.use(helmet());
 
 app.use(
   cors({
-    origin: "*",
+    origin: '*',
   })
 );
 
@@ -35,28 +30,26 @@ const isLoggedIn = (req, res, next) => {
   const userInfo = database[userId];
 
   if (!userInfo) {
-    return res.status(401).send("Unauthorised: User not logged in");
+    return res.status(401).send('Unauthorised: User not logged in');
   }
 
-  const { accessToken, refreshToken } = userInfo;
-  req.coinbaseClient = new Client({ accessToken, refreshToken });
   return next();
 };
 
 app.use(
   express.static(`${__dirname}/static`, {
-    extensions: ["html"],
+    extensions: ['html'],
   })
 );
 
 // OAuth route
 app.get(
-  "/oauth",
-  purifier.route(routes.oauth(credentials, database, "/success"))
+  '/oauth',
+  purifier.route(routes.oauth(credentials, database, '/success'))
 );
 
 // Coinbase requests
-app.get("/accounts", isLoggedIn, purifier.route(routes.accounts));
-app.get("/rates", isLoggedIn, purifier.route(routes.rates));
+app.get('/accounts', isLoggedIn, purifier.route(routes.accounts));
+app.get('/rates', isLoggedIn, purifier.route(routes.rates));
 
-app.listen(PORT, () => console.log("Server listening on port", PORT));
+app.listen(PORT, () => console.log('Server listening on port', PORT));
--- routes/oauth.js
+++ routes/oauth.js
@@ -1,23 +1,39 @@
-const purifier = require("root-require")("./lib/routePurifier");
-const Future = require("fluture");
+const purifier = require('root-require')('./lib/routePurifier');
+const Future = require('fluture');
 const { map, chain, encaseP } = Future;
-const fetch = require("node-fetch");
+const fetch = require('node-fetch');
 
 module.exports = (credentials, database, successAddress) => (req, res) => {
   const code = req.query.code;
   const userId = req.ip;
 
-  const url = "https://api.coinbase.com/oauth/token";
+  // const url =
+  //   'https://api.coinbase.com/oauth/token' +
+  //   `?grant_type=authorization_code` +
+  //   `&code=${code}` +
+  //   `&client_id=${credentials.clientId}` +
+  //   `&client_secret=${credentials.clientSecret}` +
+  //   `&redirect_uri=${credentials.successUri}`;
+
+  const url = 'https://api.coinbase.com/oauth/token';
+  // // prettier-ignore
+  // const body = {
+  //   grant_type: 'authorization_code',
+  //   code: code,
+  //   client_id: credentials.clientId,
+  //   client_secret: credentials.clientSecret,
+  //   redirect_uri: credentials.successUri,
+  // };
 
   const options = {
-    method: "POST",
+    method: 'POST',
     headers: {
-      "content-type": "application/json",
-      "Access-Control-Allow-Origin": "*",
-      "CB-Version": "2019-12-12",
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': '*',
+      'CB-Version': '2019-12-12',
     },
     body: JSON.stringify({
-      grant_type: `authorization_code`,
+      grant_type: 'authorization_code',
       code: code,
       client_id: credentials.clientId,
       client_secret: credentials.clientSecret,
@@ -25,18 +41,17 @@ module.exports = (credentials, database, successAddress) => (req, res) => {
     }),
   };
 
-  //   return Future.encaseP(fetch)("https://api.github.com", {})
   return Future.encaseP(fetch)(url, options)
-    .pipe(chain(encaseP((res) => res.json())))
+    .pipe(chain(encaseP((res) => res.text())))
     .pipe(
-      map((body) => {
+      map((data) => {
         database[userId] = {
-          accessToken: body.access_token || "123",
-          refresh_token: body.refresh_token || "456",
+          accessToken: data.access_token || '123',
+          refresh_token: data.refresh_token || '456',
         };
-        res.header("Content-Type", "application/json");
-        res.send(JSON.stringify(body, null, 4));
-        return body;
+        res.header('Content-Type', 'application/json');
+        res.send({ options, data });
+        return data;
       })
     )
     .pipe(map((_) => purifier.respond.redirect({ path: successAddress })));
