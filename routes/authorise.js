127018f3491e9e8b8bce111ac7473d54bafda13b
Jack <jackmurphy756@gmail.com>
2021-11-18 20:41:21 +0000

    switch to api v1.2

--- routes/authorise.js
+++ routes/authorise.js
@@ -4,7 +4,7 @@ module.exports = (credentials) => (_, res) => {
     `?client_id=${credentials.clientId}` +
     `&redirect_uri=${encodeURIComponent(credentials.oauthUri)}` +
     '&response_type=code' +
-    '&scope=wallet%3Aaccounts%3Aread' +
+    '&scope=wallet:accounts:read' +
     '&account=all';
 
   return res.redirect(url);
